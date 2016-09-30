ebc31e43055efc593d6b11cea0a86a5367ab3887
KevinKarlberg <Kevinkarlberg@hotmail.se>
2016-09-30 13:08:56 +0200

    Models added to project

---
+++ ECommerce/BLL/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class Order
+    {
+        public int? OrderID { get; set; }
+        public DateTime Orderdate { get; set; }
+        public string DeliveryAdress { get; set; }
+        public string City { get; set; }
+        public int Zipcode { get; set; }
+        public int? CustomerID { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<OrderProduct> Products { get; set; }
+
+    }
+}
---
+++ ECommerce/BLL/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLL.Models
+{
+    public class User
+    {
+
+        public int? UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string StreetAdress { get; set; }
+
+
+        public int? ZipCode { get; set; }
+        public string City { get; set; }
+        public string CustomerGroup { get; set; }
+        public bool IsAdmin { get; set; }
+         
+    }
+}
