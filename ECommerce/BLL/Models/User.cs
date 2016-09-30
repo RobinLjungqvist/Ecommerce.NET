265af0cbebda0be83d1bdcb553e016bf11a87898
RobinLjungqvist <robin.ljungqvist@gmail.com>
2016-09-30 17:03:21 +0200

    Registration and InputHelper

--- ECommerce/BLL/Models/User.cs
+++ ECommerce/BLL/Models/User.cs
@@ -14,6 +14,7 @@ namespace BLL.Models
         public string UserName { get; set; }
         public string Password { get; set; }
         public string StreetAdress { get; set; }
+        public string Email { get; set; }
 
 
         public int? ZipCode { get; set; }
