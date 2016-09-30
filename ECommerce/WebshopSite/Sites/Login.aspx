6235e37d19ae8ac8f49d5661a8a24fd871713022
RobinLjungqvist <robin.ljungqvist@gmail.com>
2016-09-30 13:48:26 +0200

    basic login

--- ECommerce/WebshopSite/Sites/Login.aspx
+++ ECommerce/WebshopSite/Sites/Login.aspx
@@ -1,6 +1,19 @@
 ﻿<%@ Page Title="Login" MasterPageFile="~/Site.Master" Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebshopSite.Sites.Login" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div id="LoginContent" runat="server">
+
+        <asp:Label ID="lbl_username" runat="server" Text="Username:"></asp:Label>
+        <asp:TextBox ID="txtbox_username" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="reqfieldval_username" runat="server" ControlToValidate="txtbox_username" ErrorMessage="* You must enter a username." ForeColor="Red"></asp:RequiredFieldValidator>
+        <br />
+        <asp:Label ID="lbl_password" runat="server" Text="Password:"></asp:Label>
+        <asp:TextBox ID="txtbox_password" runat="server" TextMode="Password"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="reqfieldval_password" runat="server" ErrorMessage="* You must enter a password." ForeColor="Red"></asp:RequiredFieldValidator>
+        <br />
+        <asp:Button ID="btn_login" runat="server" Text="Login" />
+        <asp:Label ID="lbl_loginerror" runat="server" ForeColor="Red" Text="Incorrect username or password." Visible="False"></asp:Label>
+
+    </div>
 
 </asp:Content>
-
--- ECommerce/WebshopSite/Sites/Login.aspx.cs
+++ ECommerce/WebshopSite/Sites/Login.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using BLL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +9,39 @@ using System.Web.UI.WebControls;
 
 namespace WebshopSite.Sites
 {
-    public partial class Login : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+	public partial class Login : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+            if(Session["User"] != null)
+            {
+                var username = txtbox_username.Text;
+                var password = txtbox_password.Text;
+                var bll = new BLLUser();
+                var user = new User();
+                var users = new List<User>();
 
-        }
-    }
+                user.UserName = username;
+                user.Password = password;
+
+                users = bll.SearchUser(user);
+
+                if(users.Count == 1)
+                {
+                    Session["User"] = users[0];
+                }
+                else
+                {
+                    txtbox_username.Text = string.Empty;
+                    txtbox_password.Text = string.Empty;
+                    lbl_loginerror.Visible = true;
+                }
+
+            }
+            else
+            {
+                Response.Redirect("~/MyAccount.aspx");
+            }
+		}
+	}
 }
