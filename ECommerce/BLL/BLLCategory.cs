055bc01f48f2c1b4af65db5a0944239cd4c9b483
KevinKarlberg <Kevinkarlberg@hotmail.se>
2016-09-30 17:02:14 +0200

    BLLCategory first method done

---
+++ ECommerce/BLL/BLLCategory.cs
@@ -0,0 +1,26 @@
+﻿using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using System.Data;
+
+namespace BLL
+{
+    class BLLCategory
+    {
+        public List<string> ReturnAllCategories()
+        {
+            var dal = new DALGeneral();
+            var datatable = dal.GetData("Select * from tblCategory");
+            var stringlist = new List<string>();
+            foreach (DataRow row in datatable.Rows)
+            {
+                stringlist.Add($"{row["Category"]}");
+            }
+            return stringlist;
+        }
+    }
+}
