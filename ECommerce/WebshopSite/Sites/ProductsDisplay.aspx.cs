598ed3c996a00a388d0b09c80fcfa3c9a6dbd24c
KevinKarlberg <Kevinkarlberg@hotmail.se>
2016-10-01 12:04:42 +0200

    Adding structure and style, you're a wizard harry!

--- ECommerce/WebshopSite/Sites/ProductsDisplay.aspx.cs
+++ ECommerce/WebshopSite/Sites/ProductsDisplay.aspx.cs
@@ -44,6 +44,63 @@ namespace WebshopSite.Sites
                                          $"</div>" +
                                          $"";
             }
+            foreach (var item in productList)
+            {
+                html += $"<div class=\"col-md-3 col-sm-6 productdisplay\">" +
+                                            $"<div class=\"single-shop-product\">" +
+                                            $"<div class=\"product-upper\">" +
+                                            $"<img src = \"../Images/testimage.png\" alt=\"image\">" +
+                                            $"</div>" +
+                                            $"<h2><a href = \"\" > {item.name}</a></h2>" +
+                                            $"<div class=\"product-carousel-price\">" +
+                                            $"<ins>{Convert.ToInt32(item.ppu)}kr</ins>" +
+                                            $"</div>" +
+
+                                            $"<div class=\"product-option-shop\">" +
+                                            $"<a class=\"add_to_cart_button\" data-quantity=\"1\" data-product_sku=\"\" data-product_id=\"70\" rel=\"nofollow\" href=\"/canvas/shop/?add-to-cart=70\">Add to cart</a>" +
+                                            $"</div>" +
+                                            $"</div>" +
+                                            $"</div>" +
+                                            $"";
+            }
+            foreach (var item in productList)
+            {
+                html += $"<div class=\"col-md-3 col-sm-6 productdisplay\">" +
+                                            $"<div class=\"single-shop-product\">" +
+                                            $"<div class=\"product-upper\">" +
+                                            $"<img src = \"../Images/testimage.png\" alt=\"image\">" +
+                                            $"</div>" +
+                                            $"<h2><a href = \"\" > {item.name}</a></h2>" +
+                                            $"<div class=\"product-carousel-price\">" +
+                                            $"<ins>{Convert.ToInt32(item.ppu)}kr</ins>" +
+                                            $"</div>" +
+
+                                            $"<div class=\"product-option-shop\">" +
+                                            $"<a class=\"add_to_cart_button\" data-quantity=\"1\" data-product_sku=\"\" data-product_id=\"70\" rel=\"nofollow\" href=\"/canvas/shop/?add-to-cart=70\">Add to cart</a>" +
+                                            $"</div>" +
+                                            $"</div>" +
+                                            $"</div>" +
+                                            $"";
+            }
+            foreach (var item in productList)
+            {
+                html += $"<div class=\"col-md-3 col-sm-6 productdisplay\">" +
+                                            $"<div class=\"single-shop-product\">" +
+                                            $"<div class=\"product-upper\">" +
+                                            $"<img src = \"../Images/testimage.png\" alt=\"image\">" +
+                                            $"</div>" +
+                                            $"<h2><a href = \"\" > {item.name}</a></h2>" +
+                                            $"<div class=\"product-carousel-price\">" +
+                                            $"<ins>{Convert.ToInt32(item.ppu)}kr</ins>" +
+                                            $"</div>" +
+
+                                            $"<div class=\"product-option-shop\">" +
+                                            $"<a class=\"add_to_cart_button\" data-quantity=\"1\" data-product_sku=\"\" data-product_id=\"70\" rel=\"nofollow\" href=\"/canvas/shop/?add-to-cart=70\">Add to cart</a>" +
+                                            $"</div>" +
+                                            $"</div>" +
+                                            $"</div>" +
+                                            $"";
+            }
             ProductContainer.InnerHtml = html;
         }
     }
